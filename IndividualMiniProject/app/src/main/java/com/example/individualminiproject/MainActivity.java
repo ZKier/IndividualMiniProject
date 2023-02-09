dfc89a373241be3ecbaf525c7b9968c3351d20e4
ZKier <70117461+ZKier@users.noreply.github.com>
2023-02-09 09:04:18 -0500

    Added the project files to git, probably should've made a copy lol

---
+++ IndividualMiniProject/app/src/main/java/com/example/individualminiproject/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.individualminiproject;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_FOR_GALLERY = 1000;
+    Button button;
+    TextView textView;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3); //just typed 3 not "requestCode:3"
+
+                //ImageView imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+                //imageView.setImageBitmap(BitmapFactory.decodeFile(pathToPicture));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData(); //Uniform resource identifier points to the data you want to access
+            imageView.setImageURI(selectedImage);
+        }
+    }
+}
